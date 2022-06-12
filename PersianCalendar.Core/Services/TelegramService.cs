daca4550a92cf9fa0dde6f2c7f9a87c344eb8b09
Ali Saeidi <alisaeidi.windows10@gmail.com>
2022-06-12 20:19:23 +0430

    Now Bot Reply To Message

--- PersianCalendar.Core/Services/TelegramService.cs
+++ PersianCalendar.Core/Services/TelegramService.cs
@@ -1,4 +1,6 @@
-﻿namespace PersianCalendar.Core.Services
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace PersianCalendar.Core.Services
 {
     public class TelegramService : ITelegramService
     {
@@ -40,15 +42,16 @@
 
             var chatId = update.Message.Chat.Id;
             var command = update.Message.Text;
+            var messageId = update.Message.MessageId;
             if (command.Contains('@'))
             {
                 command = command.Split('@')[0];
             }
             if (chatId == ChatId && LastCommand == "/prayertimes")
             {
-                await SendMessage(chatId, await oneApiService.GetPrayerTimeForCityOfIran(command.Replace("/", "")));
+                await SendMessage(chatId, await oneApiService.GetPrayerTimeForCityOfIran(command.Replace("/", "")), messageId);
             }
-            await ResponseToCommand(chatId, command);
+            await ResponseToCommand(chatId, command, messageId);
         }
 
         private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
@@ -64,26 +67,26 @@
             return Task.CompletedTask;
         }
 
-        private async Task ResponseToCommand(long chatId, string command)
+        private async Task ResponseToCommand(long chatId, string command, int messageId)
         {
             LastCommand = command;
             ChatId = chatId;
             switch (command)
             {
                 case "/occasions":
-                    await SendMessage(chatId, await CalendarService.GetOccasionsOfDay());
+                    await SendMessage(chatId, await CalendarService.GetOccasionsOfDay(), messageId);
                     break;
                 case "/omen":
-                    await SendMessage(chatId, await oneApiService.GetHafezOmen());
+                    await SendMessage(chatId, await oneApiService.GetHafezOmen(), messageId);
                     break;
                 case "/date":
-                    await SendMessage(chatId, CalendarService.GetDate());
+                    await SendMessage(chatId, CalendarService.GetDate(), messageId);
                     break;
                 case "/time":
-                    await SendMessage(chatId, CalendarService.GetTime());
+                    await SendMessage(chatId, CalendarService.GetTime(), messageId);
                     break;
                 case "/datetime":
-                    await SendMessage(chatId, CalendarService.GetDateTime());
+                    await SendMessage(chatId, CalendarService.GetDateTime(), messageId);
                     break;
                 case "/prayertimes":
                     await SendChooseCityMessage(chatId);
@@ -93,13 +96,13 @@
             }
         }
 
-        private async Task SendMessage(long chatId, string message)
+        private async Task SendMessage(long chatId, string message, int messageId)
         {
             if (message.Length > 4096)
             {
                 message = "متاسفانه خطایی رخ داده است،لطفا کمی بعد دوباره تلاش کنید";
             }
-            await botClient.SendTextMessageAsync(chatId, message);
+            await botClient.SendTextMessageAsync(chatId: chatId, text: message, replyToMessageId: messageId);
         }
 
         private async Task SendChooseCityMessage(long chatId)
